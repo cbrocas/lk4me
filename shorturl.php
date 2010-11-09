d19b88164be28a3b690dc1401c7f59cd7adbc2b1
Christophe Brocas <christophe@brocas.org>
2010-11-09 19:13:28 +0100

    GPLv3 addition.

--- shorturl.php
+++ shorturl.php
@@ -1,4 +1,23 @@
 <?php
+/*
+  Copyright 2010 Christophe Brocas
+
+  This file is part of lk4me.
+
+  lk4me is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  lk4me is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with lk4me.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
   //get url and obtain a digest of it through sha-1 algo
   $url = filter_var($_GET["url"],FILTER_VALIDATE_URL);
   
