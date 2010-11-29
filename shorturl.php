79845177c3bc68ec2adb768bb490fdb77c050fb3
Christophe Brocas <christophe@brocas.org>
2010-11-29 21:58:10 +0100

    add index.html support in .htaccess patch the way collisions are handled

--- shorturl.php
+++ shorturl.php
@@ -25,61 +25,71 @@
 
   echo "Your URL [".$url."]<br/>";
   
-  // extract from the digest the firt 2 chars and the first 6 characters
+  // extract the path to the script from the URL
+  // to generate the path to the short URL
+  // Example 1 : 
+  // . url = http://domain.tld/shorturl.php?url=xxx
+  // . baseurl = http://domain.tld/
+  // Example 2 :
+  // . url = http://domain.tld/test/shorturl.php?url=xxx
+  // . baseurl = http://domain.tld/test/
+
+  $baseurl = "http://".$_SERVER['HTTP_HOST']."/";
+  $tmp = explode("/",$_SERVER['SCRIPT_NAME']);
+  $count = count($tmp) - 1;
+
+  for ($i=1;$i<$count;$i++) {
+       $baseurl = $baseurl . $tmp[$i] ."/";
+  }
+  
   $shift = 0;
 
-  $character1 = substr($hash, $shift , 1);
-  $character2 = substr($hash, $shift+1, 1);
-  $hash6characters = substr($hash, $shift, 6);
-
-  $dirpath = $character1."/".$character2;
-  $filepath = $dirpath."/".$hash6characters;
-
-  // if the directories of the filepath do not exist, create them
-  if (!is_dir($dirpath)) 
-      mkdir($dirpath, 0700, true);
-
-  if (is_file($filepath)) {
-      $f = fopen($filepath,"r");
-      $urlinfile = fgets($f,16000);
-      fclose($f); 
-
-      if ($urlinfile == $url) {
-          echo "- SHORT URL (already known): [".$shorturl."]<br/>";
-      }
-      else {
-	      echo "collision !";
-	      exit(1);
-      }
+  while($shift < 6) {
+      // extract from the digest the firt 2 chars and the first 6 characters
+      $character1 = substr($hash, $shift , 1);
+      $character2 = substr($hash, $shift+1, 1);
+      $hash6characters = substr($hash, $shift, 6);
       
-  }
-  else {
+      $dirpath = $character1."/".$character2;
+      $filepath = $dirpath."/".$hash6characters;
 
-      // extract the path to the script from the URL
-      // to generate the path to the short URL
-      // Example 1 : 
-      // . url = http://domain.tld/shorturl.php?url=xxx
-      // . shorturl = http://domain.tld/
-      // Example 2 :
-      // . url = http://domain.tld/test/shorturl.php?url=xxx
-      // . shorturl = http://domain.tld/test/
-
-      $shorturl = "http://".$_SERVER['HTTP_HOST']."/";
-      $tmp = explode("/",$_SERVER['SCRIPT_NAME']);
-      $count = count($tmp) - 1;
-
-      for ($i=1;$i<$count;$i++) {
-           $shorturl = $shorturl . $tmp[$i] ."/";
-      }
+      // if the directories of the filepath do not exist, create them
+      if (!is_dir($dirpath)) 
+          mkdir($dirpath, 0700, true);
 
       // add the six chars long hash to the shorturl
-      $shorturl = $shorturl . $hash6characters;
+      $shorturl = $baseurl . $hash6characters;
 
-      $f = fopen($filepath,"w+");
-      fputs($f, $url);
-      fclose($f);
+      if (is_file($filepath)) {
+          $f = fopen($filepath,"r");
+          $urlinfile = fgets($f,16000);
+          fclose($f); 
+
+          if ($urlinfile == $url) {
+              echo "- SHORT URL (already known): [".$shorturl."] shift:".$shift;
+              break;
+          }
+          else {
+	     // collision : first 6 characters of the sha-1 hash of the received URL
+             //             are already used by a 6 char. long hash of another URL. We shift 
+             //             for one character on the right of the sha-1 hash of the 
+             //             received URL and retry. We do this up to 6 times.
+	     $shift=$shift+1;
+         }
+      
+      }
+      else {
+          $f = fopen($filepath,"w+");
+          fputs($f, $url);
+          fclose($f);
   
-      echo "- SHORT URL (new): [" .$shorturl."]<br/>";
+          echo "- SHORT URL (new): [" .$shorturl."] shift:".$shift."<br/>";
+          break;
+      }
   }
-
+  
+  if ($shift==6) { 
+      echo "ERROR: Unable to generate a free value of 6 characters for the short URL. Sorry";
+      echo "[".$hash."]";
+  }
 ?>
