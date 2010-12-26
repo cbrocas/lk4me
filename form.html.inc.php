85677d7b24b04a93e45891e4aa8bc3b3cbb21be0
Christophe Brocas <christophe@brocas.org>
2010-12-26 13:58:22 +0100

    initial import.

---
+++ form.html.inc.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  Copyright 2010 Christophe Brocas
+
+  This file is part of lk4me.
+
+  lk4me is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  lk4me is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with lk4me.  If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
+
+<form id="url" name="URL" action="shorturl.php" method="GET" enctype="application/x-www-form-urlencoded" novalidate="novalidate">
+
+Your long URL :<br>
+<input type="url" name="url" size="40" value="<?php echo $url; ?>" >
+<button type="submit" name="go">shorten !</button>
+<span class="red"><?php echo $shorturl;?></span>
+</form>  
