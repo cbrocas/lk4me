32ac0186753d76ff36572a22f43546d4583b77ab
Christophe Brocas <christophe@brocas.org>
2010-12-26 12:59:30 +0100

    change to non absolute links and fron index.html to index.php

---
+++ lk4me.html.inc.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html><head>
+<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz' rel='stylesheet' type='text/css'>    
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+<title>link for me. your URL shortener :)</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="icon" type="image/jpeg" href="images/favicon2.jpg" />
+</head>
+
+<body >
+
+<div id="topnav">
+  <div class="topnav-wrapper">
+    <ul>
+        <li><a href="index.php">home</a></li>
+	<li><a href="https://github.com/cbrocas/lk4me">code</a></li>
+	<li><a href="https://github.com/cbrocas/lk4me/archives/master">download</a></li>
+	<li><a href="about/">about</a></li>
+    </ul>
+  </div>
+</div>
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
