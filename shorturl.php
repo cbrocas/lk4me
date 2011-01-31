8f5a951b1ac69286ed9e560c1322c1c686f72d55
Christophe Brocas <christophe@brocas.org>
2011-01-31 21:46:21 +0100

    add generateShortURI function.

--- shorturl.php
+++ shorturl.php
@@ -23,55 +23,19 @@
 
   // filter url received through Query String
   $url = filter_var($_GET["url"],FILTER_SANITIZE_URL);
-  
-  $hash = sha1($url);
+
+  $hash = sha1($url); 
   $baseurl = getURIbase();
-  $shift = 0;
 
-  while($shift < 6) {
-      // extract from the digest the firt 2 chars and the first 6 characters
-      $character1 = substr($hash, $shift , 1);
-      $character2 = substr($hash, $shift+1, 1);
-      $hash6characters = substr($hash, $shift, 6);
-      
-      $dirpath = $character1."/".$character2;
-      $filepath = $dirpath."/".$hash6characters;
-
-      // if the directories of the filepath do not exist, create them
-      if (!is_dir($dirpath)) 
-          mkdir($dirpath, 0700, true);
-
-      // add the six chars long hash to the shorturl
-      $shorturl = $baseurl . $hash6characters;
-
-      if (is_file($filepath)) {
-          $urlinfile = file_get_contents($filepath);
-          
-          if ($urlinfile == $url) {
-              break;
-          }
-          else {
-	     // collision : first 6 characters of the sha-1 hash of the received URL
-             //             are already used by a 6 char. long hash of another URL. We shift 
-             //             for one character on the right of the sha-1 hash of the 
-             //             received URL and retry. We do this up to 6 times.
-	     $shift=$shift+1;
-         }
-      
-      }
-      else {
-          $f = fopen($filepath,"w+");
-          fputs($f, $url);
-          fclose($f);
-          break;
-      }
-  }
-  
-  if ($shift==6) { 
+  // display error message if unable to generate a short url for a long one
+  if (!generateShortURI($hash, $baseurl, &$shorturl))
+  { 
       echo "<span class=\"red\">ERROR: Unable to generate a free value of 6 characters for the short URL. Sorry</span> <br/>";
 
   }
-  require('form.html.inc.php');
+  // if we succeed to generate the shorturl, the shorturl var set 
+  // by the function will be displayed.
+  require('html/form.html.inc.php');
 ?>
 </body>
 </html>
